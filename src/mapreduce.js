ee3d2e6ddb9730096494ccf114579715c4424a1b
Jonathan Prins <jonprins@gmail.com>
2009-10-22 17:35:37 -0400

    added checking for the presence of Array.prototype.reduce, defining it as per MDC if it is undefined.

--- src/mapreduce.js
+++ src/mapreduce.js
@@ -31,7 +31,7 @@ MapReduce.prototype = {
 	}
     },
     start : function (data) {
-	// execute	
+	// execute   
     },
     finish : function (result) {
 	if (typeof callback_scope == "object") {
@@ -42,3 +42,52 @@ MapReduce.prototype = {
 	}
     }	
 };
+
+
+
+// reduce function defined if undefined, as per MDC
+// ** https://developer.mozilla.org/En/Core_JavaScript_1.5_Reference/Global_Objects/Array/Reduce
+
+if (!Array.prototype.reduce)
+{
+  Array.prototype.reduce = function(fun /*, initial*/)
+  {
+    var len = this.length >>> 0;
+    if (typeof fun != "function")
+      throw new TypeError();
+
+    // no value to return if no initial value and an empty array
+    if (len == 0 && arguments.length == 1)
+      throw new TypeError();
+
+    var i = 0;
+    if (arguments.length >= 2)
+    {
+      var rv = arguments[1];
+    }
+    else
+    {
+      do
+      {
+        if (i in this)
+        {
+          rv = this[i++];
+          break;
+        }
+
+        // if array contains no values, no initial value to return
+        if (++i >= len)
+          throw new TypeError();
+      }
+      while (true);
+    }
+
+    for (; i < len; i++)
+    {
+      if (i in this)
+        rv = fun.call(null, rv, this[i], i, this);
+    }
+
+    return rv;
+  };
+}
