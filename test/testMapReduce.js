36210972a229b8e89169ab1edf24b13935ff8592
Jonathan Prins <jonprins@gmail.com>
2009-10-22 17:16:26 -0400

    tests for valid and invalid args to setMap

--- test/testMapReduce.js
+++ test/testMapReduce.js
@@ -3,15 +3,19 @@ eval(loadFile("src/mapreduce.js"));
 var mr;
 
 testCases(test,
-  function setUp() {
-      mr = new MapReduce();
-  },
-  function testSetMapWithValidArgs() {
-    var map = function () { var foo = "foo"; };
-    mr.setMap(map);
-    assert.that(mr.map, eq(map));
-  },
-  function tearDown() {
-      mr = null;
-  }
+	  function setUp() {
+	      mr = new MapReduce();
+	  },
+	  function testSetMapWithValidArgs() {
+	      var map = function () { var foo = "foo"; };
+	      mr.setMap(map);
+	      assert.that(mr.map, eq(map));
+	  },
+	  function testSetMapWithInvalidArgs() {
+	      shouldThrowException(function() { mr.setMap("foo");},
+				   "Invalid args should throw.");
+	  },
+	  function tearDown() {
+	      mr = null;
+	  }
 );
